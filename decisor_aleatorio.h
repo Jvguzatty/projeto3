eebc8ccbfbc34e942365a3da3c7033285e7c3589
thiagomacedoreis-design <thiagomacedoreis@alunos.utfpr.edu.br>
2026-06-22 12:41:59 -0300

    Update decisor_aleatorio.h

--- decisor_aleatorio.h
+++ decisor_aleatorio.h
@@ -3,32 +3,80 @@
 /*----------------------------------------------------------------------------*/
 /* Autor: Bogdan T. Nassu                                                     */
 /*============================================================================*/
-/** Um rob� muito ruim - ele decide seus movimentos aleatoriamente! */
+/** Um robô muito ruim - ele decide seus movimentos aleatoriamente! */
 /*============================================================================*/
 
-#ifndef __DECISOR_ALEATORIO_H
-#define __DECISOR_ALEATORIO_H
+#include <stdlib.h>
+
+#include "t3-x-y-z.h"
 
 /*============================================================================*/
+/** Simplesmente aloca um Decisor.
+ *
+ * Parâmetros: int altura: número de linhas no tabuleiro.
+ *             int largura: número de colunas no tabuleiro.
+ *
+ * Valor de Retorno: um Decisor alocado. */
 
-#include "game_state.h"
-
-/*============================================================================*/
-/** Estrutura do decisor. Este � um decisor muito burro, n�o tem nada... */
-
-typedef struct
+Decisor* criaDecisor (int altura, int largura)
 {
-    int reconhecer_spawn;
+    Decisor* d = ((Decisor*) malloc ((altura*largura)*sizeof (Decisor)));
+    d->m = (int**)malloc(altura*sizeof(int*));
+    for(int i = 0;i<largura;i++){
+        d->m[i] = (int*)malloc(largura*sizeof(int));
+    }
+    return d;
+}
 
-} Decisor;
+/*----------------------------------------------------------------------------*/
+/** Simplesmente desaloca um Decisor.
+ *
+ * Parâmetros: Decisor* d: Decisor a destruir.
+ *
+ * Valor de Retorno: NENHUM */
+
+void destroiDecisor (Decisor* d)
+{
+    free (d);
+}
+
+/*----------------------------------------------------------------------------*/
+/** Este decisor não faz absolutamente nada quando é informado sobre o conteúdo
+ * da posição atual! Ele simplesmente retorna um movimento aleatório.
+ *
+ * Parâmetros: Decisor* d: o Decisor.
+ *             Coordenada pos: posição atual.
+ *             int agua: 1 se a posição atual tiver água, 0 do contrário.
+ *             int n_lava: número de poços de lava em casas vizinhas à atual.
+ *
+ * Valor de Retorno: a direção do próximo movimento: 1 – para cima, 2 – para baixo, 3 – para a esquerda, 4 – para a direita. */
+
+int proximoMovimento (Decisor* d, Coordenada pos, int agua, int n_lava)
+{
+
+
+//////////////////////////////////////////////////////////
+    //reconhecer spawn, pq nao pode ter lava aos redos dele
+    if (d->reconhecer_spawn == 0)
+    {
+        if (pos.x == 0 && pos.y == 0) return (4);
+        if (pos.x == 1 && pos.y == 0) return (2);
+        if (pos.x == 1 && pos.y == 1) return (3);
+//              Se a memoria diz que ele AINDA NN terminou o spawn (vale 0)
+        if (pos.x == 0 && pos.y == 1)
+        {
+            d->reconhecer_spawn = 1;
+            return (1);
+        }
+    }
+
+    if(agua == 1){
+
+    }
+////////////////////////////////////////////////////////////
+    return (2);
+
+
+}
 
 /*============================================================================*/
-
-Decisor* criaDecisor (int altura, int largura);
-void destroiDecisor (Decisor* d);
-int proximoMovimento (Decisor* d, Coordenada pos, int agua, int n_lava);
-
-/*============================================================================*/
-#endif /* __DECISOR_ALEATORIO_H */
-
-
